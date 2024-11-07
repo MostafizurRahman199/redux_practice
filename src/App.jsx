9bae59ecee53092e446faf27729a47103ab6424b
MostafizurRahman199 <fardilshifat199@gmail.com>
2024-11-07 10:05:18 +0600

    home design done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "./components/Home";
+import HomeContainer from "./Container/HomeContainer";
+
+function App() {
+  return (
+    <div>
+      <HomeContainer />
+    </div>
+  );
+}
+
+export default App;
